9a4735207255dc828b992011a3639d3426b3c15d
r-h <r-h@users.noreply.github.com>
2014-06-15 19:38:09 +0200

    Added bootstrap in UI

--- src/main/java/io/github/r_h/simple_web_calc/servlet/NumberWordsWebCalcServlet.java
+++ src/main/java/io/github/r_h/simple_web_calc/servlet/NumberWordsWebCalcServlet.java
@@ -14,34 +14,48 @@ import org.apache.log4j.Logger;
 /**
  * Servlet implementation class SimpleWebCalcServlet
  */
-public class SimpleWebCalcServlet extends HttpServlet {
+public class NumberWordsWebCalcServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	private static final Logger LOG = Logger
-			.getLogger(SimpleWebCalcServlet.class);
+			.getLogger(NumberWordsWebCalcServlet.class);
 
 	private static final String EXPRESSION_PARAMETER_KEY = "expressions";
 	
 	private static final String INITIAL_DEMO_EXPRESSIONS = "5 + 6\nsechs durch drei\nvier mal einhundertvierundachtzigtausendzweihundertzweiundvierzig";
 
 	private static final String STATIC_HTML = ""
-			+ "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">\n"
+			+ "<!doctype html>\n"
 			+ "<html>\n"
-			+ "	<head><title>SimpleWebCalc, a simple web calculator.</title>\n"
-			+ "   <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">"
-			+ "   </head>"
+			+ "	<head>"
+			+ "   <meta charset=\"utf-8\">"
+			+ "   <title>NumberWordsWebCalc, a calculator that can process German number words.</title>\n"
+			+ "   <meta content=\"IE=edge,chrome=1\" http-equiv=\"X-UA-Compatible\">"
+			+ "   <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"
+			+ "   <link href=\"//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css\" rel=\"stylesheet\">"
+			+ " </head>"
 			+ "	<body>\n"
-			+ "	<h1>SimpleWebCalc</h1><br />\n"
-			+ "	<h2>Please enter a simple expression either numerical or in german words e.g. '3 + 4' or 'vierhundert mal 2'.</h2><br />\n"
-			+ "	<h3>Each expression can have 2 operands (integer numbers starting from -9.999.999 to 9.999.999 in german words numerical) and an operator (+, -, *, / or in german words e.g. 'mal', 'durch').</h3><br />\n"
-			+ "	<h3>Operands and operators have to be sperated by space characters.</h3><br />\n"
-			+ "	<h4>Each line can contain one expression, you can enter multiple lines.</h4><br />\n"
-			+ "   <font color=\"#CC0000\" size=\"2\">%ERRORS%</font><br />"
-			+ "	<b>Result is :%RESULT%</b><br />"
-			+ "	<form method=\"post\" accept-charset=\"UTF-8\">\n"
-			+ "	<textarea name=\"expressions\" rows=\"10\" cols=\"100\">%EXPRESSIONS%</textarea><br />\n"
-			+ "	<input type=\"submit\" name=\"submit\" value=\"Submit\" /></form>\n"
-			+ "	</body>\n" + "</html>\n";
+			+ "	<div class=\"container\" role=\"main\">\n"
+			+ "  <div class=\"jumbotron\">"
+			+ "	  <h1>NumberWordsCalc</h1>"
+			+ "	  <h3>Please enter a simple expression either numerical or in german words e.g. '3 + 4' or 'vierhundert mal 2'.</h3>\n"
+			+ "	  <h4>Each expression can have 2 operands (integer numbers starting from -9.999.999 to 9.999.999 in german words numerical) and an operator (+, -, *, / or in german words e.g. 'mal', 'durch').</h4>\n"
+			+ "	  <h5>Operands and operators have to be sperated by space characters.</h5>\n"
+			+ "	  <h6>Each line can contain one expression, you can enter multiple lines.</h6>\n"
+			+ "   <p class=\"bg-danger\">%ERRORS%</p>"
+			+ "	  <b>Result is :%RESULT%</b><br />"
+			+ "	  <form method=\"post\" accept-charset=\"UTF-8\" role=\"form\">\n"
+			+ "      <div class=\"form-group\">"
+			+ "	       <textarea name=\"expressions\" class=\"form-control\">%EXPRESSIONS%</textarea><br />\n"
+			+ "      </div>"
+			+ "      <div class=\"form-group\">"
+			+ "	       <button type=\"submit\" class=\"btn btn-primary btn-lg\" role=\"button\">Submit</button>"
+			+ "      </div>"
+			+ "   </form>\n"
+			+ "  </div>\n"
+			+ "	</div>\n"
+			+ "	</body>\n" 
+			+ "</html>\n";
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
